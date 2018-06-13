c648c6fea92f7a54f54cb8bf68ba56ea3fa4921d
g1ts <coder@g1ts.net>
2018-06-13 16:39:49 +0500

    dev

--- www/js/index.js
+++ www/js/index.js
@@ -13,7 +13,22 @@ var app = {
       }catch(e){}
       try{
         //AndroidFullScreen.immersiveMode();
-        AndroidFullScreen.setSystemUiVisibility(8);
+        //AndroidFullScreen.setSystemUiVisibility(AndroidFullScreen.SYSTEM_UI_FLAG_FULLSCREEN | AndroidFullScreen.SYSTEM_UI_FLAG_LOW_PROFILE);
+        /*
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        */
+        AndroidFullScreen.setSystemUiVisibility(
+            AndroidFullScreen.SYSTEM_UI_FLAG_LAYOUT_STABLE
+          | AndroidFullScreen.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          | AndroidFullScreen.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+          | AndroidFullScreen.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | AndroidFullScreen.SYSTEM_UI_FLAG_FULLSCREEN
+          | AndroidFullScreen.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
       }catch(e){}
     }
 };
