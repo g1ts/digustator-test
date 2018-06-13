9188b16bae4336032fde3855d0f629a4e00b5bb4
g1ts <coder@g1ts.net>
2018-06-13 14:43:36 +0500

    dev

--- www/js/index.js
+++ www/js/index.js
@@ -2,11 +2,13 @@
 var app = {
     initialize: function() {
         this.bindEvents();
-        try{
-          window.plugins.insomnia.keepAwake(function(){alert('ok')});
-        }catch(e){
-          alert('insomnia error', e)
-        }
+        setTimeout(function(){
+          try{
+            window.plugins.insomnia.keepAwake(function(){alert('ok')});
+          }catch(e){
+            alert('insomnia error', e)
+          }
+        }, 5000)
     },
     bindEvents: function() {
       document.addEventListener("backbutton", function(e) {e.preventDefault();}, false);
